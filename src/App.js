636b81cb6a2179e804052b8be994f10fa9791526
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-04-14 22:50:35 -0300

    useEffect e api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const a = window.localStorage.getItem('produto');
+    if (a !== null) {
+      setProduto(a);
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (produto !== null) {
+      window.localStorage.setItem('produto', produto)
+    }
+    //window.localStorage = acessar o local storage, .setItem=criar um item dentro do local storage, ('produto', produto)= definir o nome da chave como 'produto' e valor produto
+  }, [produto])
+
+  const handleClick = (event) => {
+    setProduto(event.target.innerHTML);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CEP selecionado:{produto}</h1>
+      <button onClick={handleClick} style={{ margin: '0 10px' }}>
+        35700291
+      </button>
+      <button onClick={handleClick} style={{ margin: '0 10px' }}>
+        35700357
+      </button>
     </div>
   );
 }
